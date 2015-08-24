257255dba041f8cd43e404b05e2650fb1cdd3856
hqlgree2 <hqlgree2@gmail.com>
2015-08-24 22:06:35 +0800

    controlling tick spacing/labeling;multiple figures

--- python/p.matplotlib/30.shape.py
+++ python/p.matplotlib/30.shape.py
@@ -1,7 +1,10 @@
 """adding shapes"""
 
+import numpy as np
+import datetime
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 def plot1():
     """plot1"""
@@ -13,7 +16,7 @@ def plot1():
 
     shape = patches.Ellipse((0, -2.), 2., 1., angle=45., color='.75')
     plt.gca().add_patch(shape)
-    
+
     # boxstyle => larrow; rarrow; round; round4; roundtooth; sawtooth; square
     shape = patches.FancyBboxPatch((2.5, -2.5), 2., 1., boxstyle='roundtooth', color='.75')
     plt.gca().add_patch(shape)
@@ -22,4 +25,101 @@ def plot1():
     plt.axis('scaled')
     plt.show()
 
-plot1()
+def plot2():
+    """working with polygons"""
+    # theta = np.linspace(0, 2 * np.pi, 8)
+    theta = np.linspace(0, 2 * np.pi, 9)
+    # theta = np.linspace(0, 2 * np.pi, 10)
+    # points = np.vstack((np.cos(theta), np.sin(theta))).transpose()
+    points = np.vstack((np.sin(theta), np.cos(theta))).transpose()
+
+    plt.gca().add_patch(patches.Polygon(points, color='.75'))
+    plt.grid(True)
+    plt.axis('scaled')
+    plt.show()
+
+def plot3():
+    """working with path attributes"""
+    theta = np.linspace(0, 2 * np.pi, 6)
+    points = np.vstack((np.sin(theta), np.cos(theta))).transpose()
+    plt.gca().add_patch(plt.Circle((0, 0), radius=1., color='.75'))
+    plt.gca().add_patch(plt.Polygon(points, closed=None, fill=None, lw=3., ls='dashed', edgecolor='k'))
+    plt.grid(True)
+    plt.axis('scaled')
+    plt.show()
+
+def plot4():
+    """controlling tick spacing"""
+    X = np.linspace(-15, 15, 1024)
+    Y = np.sinc(X)
+
+    ax = plt.axes()
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(5))
+    ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
+
+    plt.grid(True, which='both')
+    plt.plot(X, Y, c='k')
+    plt.show()
+
+def plot5():
+    """controlling tick labeling"""
+    name_list = ('omar', 'serguey', 'max', 'zhou', 'abidin')
+    value_list = np.random.randint(0, 99, size=len(name_list))
+    pos_list = np.arange(len(name_list))
+
+    ax = plt.axes()
+    ax.xaxis.set_major_locator(ticker.FixedLocator((pos_list)))
+    ax.xaxis.set_major_formatter(ticker.FixedFormatter(name_list))
+
+    plt.bar(pos_list, value_list, color='.75', align='center')
+    plt.show()
+
+def plot6():
+    """simpler way to create bar charts with fixed labels"""
+    name_list = ('omar', 'serguey', 'max', 'zhou', 'abidin')
+    value_list = np.random.randint(0, 99, size=len(name_list))
+    pos_list = np.arange(len(name_list))
+
+    plt.bar(pos_list, value_list, color='.75', align='center')
+    plt.xticks(pos_list, name_list)
+    plt.show()
+
+def make_label7(value, pos):
+    """make_label7"""
+    return '%0.1f%%' % (100. * value)
+
+def plot7():
+    """advanced label generation"""
+    ax = plt.axes()
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(make_label7))
+    X = np.linspace(0, 1, 256)
+    plt.plot(X, np.exp(-10 * X), c='k')
+    plt.plot(X, np.exp(-5 * X), c='k', ls='--')
+    plt.show()
+
+start_date = datetime.datetime(1998, 1, 1)
+
+def make_lable8(value, pos):
+    """make_lable8"""
+    time = start_date + datetime.timedelta(days=356 * value)
+    return time.strftime('%b %y')
+
+def plot8():
+    """delegation"""
+    ax = plt.axes()
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(make_lable8))
+    X = np.linspace(0, 1, 256)
+    plt.plot(X, np.exp(-10 * X), c='k')
+    plt.plot(X, np.exp(-5 * X), c='k', ls='--')
+    labels = ax.get_xticklabels()
+    plt.setp(labels, rotation=30.)
+    plt.show()
+
+# plot1()
+# plot2()
+# plot3()
+# plot4()
+# plot5()
+# plot6()
+# plot7()
+plot8()
---
+++ python/p.matplotlib/31.multiple.figures.py
@@ -0,0 +1,87 @@
+"""working with figures"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot1():
+    """compositing multiple figures"""
+    T = np.linspace(-np.pi, np.pi, 1024)
+    grid_size = (4, 2)
+    # define a grid of 4 rows and 2 cols
+    plt.subplot2grid(grid_size, (0, 0), rowspan=3, colspan=1)
+    plt.plot(np.sin(2 * T), np.cos(0.5 * T), c='k')
+
+    plt.subplot2grid(grid_size, (0, 1), rowspan=3, colspan=1)
+    plt.plot(np.cos(3 * T), np.sin(T), c='k')
+
+    plt.subplot2grid(grid_size, (3, 0), rowspan=1, colspan=3)
+    plt.plot(np.cos(5 * T), np.sin(7 * T), c='k')
+
+    plt.tight_layout()
+    plt.show()
+
+def get_radius(T, params):
+    """get_radius"""
+    m, n_1, n_2, n_3 = params
+    U = (m * T) / 4
+    return (np.fabs(np.cos(U)) ** n_2 + np.fabs(np.sin(U)) ** n_3) ** (-1. / n_1)
+
+def plot2():
+    """add title to each subfigure"""
+    grid_size = (3, 4)
+    T = np.linspace(0, 2 * np.pi, 1024)
+
+    for i in range(grid_size[0]):
+        for j in range(grid_size[1]):
+            params = np.random.random_integers(1, 20, size=4)
+            R = get_radius(T, params)
+
+            axes = plt.subplot2grid(grid_size, (i, j), rowspan=1, colspan=1)
+            axes.get_xaxis().set_visible(False)
+            axes.get_yaxis().set_visible(False)
+
+            plt.plot(R * np.cos(T), R * np.sin(T), c='k')
+            plt.title('%d, %d, %d, %d' % tuple(params), fontsize='small')
+
+    plt.tight_layout()
+    plt.show()
+
+def plot3():
+    """an alternative way to composite figures"""
+    T = np.linspace(-np.pi, np.pi, 1024)
+    fig, (ax0, ax1) = plt.subplots(ncols=2)
+    ax0.plot(np.sin(2 * T), np.cos(0.5 * T), c='k')
+    ax1.plot(np.cos(3 * T), np.sin(T), c='k')
+    plt.show()
+
+def plot4():
+    """scaling both the axes equally"""
+    T = np.linspace(0, 2 * np.pi, 1024)
+    # plt.plot(2. * np.cos(T), np.sin(T), c='k', lw=3.)
+    plt.plot(np.cos(T), 2. * np.sin(T), c='k', lw=3.)
+    plt.axes().set_aspect('equal')
+    plt.show()
+
+def plot5():
+    """setting an axis range"""
+    X = np.linspace(-6, 6, 2014)
+    plt.ylim(-.5, 1.5)
+    plt.plot(X, np.sinc(X), c='k')
+    plt.show()
+
+def plot6():
+    """setting the aspect ratio"""
+    X = np.linspace(-6, 6, 1024)
+    Y1, Y2 = np.sinc(X), np.cos(X)
+
+    plt.figure(figsize=(10.24, 2.56))
+    plt.plot(X, Y2, c='.75', lw=3.)
+    plt.plot(X, Y1, c='k', lw=3.)
+    plt.show()
+
+# plot1()
+# plot2()
+# plot3()
+# plot4()
+# plot5()
+plot6()
