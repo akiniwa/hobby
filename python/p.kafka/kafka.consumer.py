94c0ba9516299ddf8b0f2ebb29159a8aa9946def
gree2 <hqlgree2@gmail.com>
2016-05-29 21:09:18 +0800

    kafka consumer and producer

---
+++ python/p.kafka/kafka.consumer.py
@@ -0,0 +1,23 @@
+#! coding: utf-8
+'''kafka-python usage'''
+
+KAFKA_SERVER = '192.168.1.30:9092'
+
+
+def kafka_consumer():
+    '''kafka consumer'''
+    from kafka import KafkaConsumer
+
+    # To consume messages
+    consumer = KafkaConsumer('topic',
+                             group_id='my-group',
+                             bootstrap_servers=[KAFKA_SERVER])
+    for message in consumer:
+        # message value is raw byte string -- decode if necessary!
+        # e.g., for unicode: `message.value.decode('utf-8')`
+        print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                             message.offset, message.key,
+                                             message.value))
+
+if __name__ == '__main__':
+    kafka_consumer()
