7e8c485c2bf239b359ef72491a717da484b162cd
gree2 <hqlgree2@gmail.com>
2016-06-20 16:50:07 +0800

    get a key not in redis

--- python/p.redis/expire.poc.py
+++ python/p.redis/expire.poc.py
@@ -3,8 +3,9 @@
 import redis
 
 conn = redis.Redis(host='192.168.99.100', port=6379, db=1)
-# False True True None
+# False None True True None
 print conn.expire('not-exist-in-redis', 10)
+print conn.get('not-exist-in-redis')
 print conn.set('exist-in-redis', 'now in redis')
 print conn.expire('exist-in-redis', 0)
 print conn.get('exist-in-redis')
